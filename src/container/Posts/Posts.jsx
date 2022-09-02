7e2a059dc4ca429842e024ace03096a5cc10978a
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-09-02 18:14:54 +0500

    I push Search part!

--- src/container/Posts/Posts.jsx
+++ src/container/Posts/Posts.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Table, Button } from "antd";
 import { useGetPostsQuery } from "../../api/posts";
+import { useState, useEffect } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 function Posts() {
   const { data = [], isLoading } = useGetPostsQuery();
+  const [search, setSearch] = useState("");
+  const [datas, setDatas] = useState([]);
 
   const columns = [
     {
@@ -24,15 +28,43 @@ function Posts() {
     },
   ];
 
+  useEffect(() => {
+    setDatas(
+      data.filter((elem) =>
+        elem.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search]);
+
   if (isLoading) return <h1>Loading</h1>;
   return (
     <div className="bg-green-400">
-        <h1 className="text-3xl font-bold pt-5 text-white">Posts Table</h1>
-      {data.length > 0 && (
+      <div className="flex justify-between w-[80%] mx-auto py-5">
+        <h1 className="text-3xl font-bold  text-white">Posts Table</h1>
+        <div>
+          <input
+            className="rounded-full px-10"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search..."
+          />
+          <AiOutlineSearch size={20} className="mt-[-30px] mx-2" />
+        </div>
+      </div>
+      {data.length > 0 && search === "" && (
         <Table
           className="w-[80%] mx-auto"
           columns={columns}
           dataSource={data}
+          rowKey="id"
+        />
+      )}
+      {search.length > 0 && data.length > 0 && (
+        <Table
+          className="w-[80%] mx-auto"
+          columns={columns}
+          dataSource={datas}
           rowKey="id"
         />
       )}
